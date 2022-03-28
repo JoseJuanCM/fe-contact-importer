f85abc00c18c6b59bf9afcdc5034457c1f06bae4
Juan De la Cruz <juan.cruz.mejia@gmail.com>
2022-03-27 21:48:32 -0600

    feat(user_options): added

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,29 @@
 <template>
-  <div>
-    <h1>Hello</h1>
+  <div class="py-6">
+    <div class="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
+      <h1 class="text-2xl font-semibold text-gray-600">
+        Welcome to Contact Import
+      </h1>
+    </div>
+    <div class="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
+      <!-- Replace with your content -->
+      <div class="py-4">
+        <div class="border-4 border-dashed border-gray-200 rounded-lg h-96 grid justify-items-center place-content-center">
+          <NuxtLink to="/contacts" class="text-3xl font-medium text-gray-600 m-4">
+            Contacts
+          </NuxtLink>
+
+          <NuxtLink to="/load" class="text-3xl font-medium text-gray-600 m-4">
+            Load New File
+          </NuxtLink>
+
+          <NuxtLink to="/files" class="text-3xl font-medium text-gray-600 m-4">
+            Previous Files
+          </NuxtLink>
+        </div>
+      </div>
+      <!-- /End replace -->
+    </div>
   </div>
 </template>
 
